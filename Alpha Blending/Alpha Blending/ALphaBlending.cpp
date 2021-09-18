9d3073722103ad181a50c3aea99e2e1343dcc723
zili-zhou <heinrichzzl@163.com>
2021-09-18 19:32:24 +0200

    Alpha Blending

--- Alpha Blending/Alpha Blending/ALphaBlending.cpp
+++ Alpha Blending/Alpha Blending/ALphaBlending.cpp
@@ -75,6 +75,10 @@ int main()
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+
 	//∂•µ„–≈œ¢
 	float vertices[] = {
 		// positions          // colors           // texture coords  //normals
